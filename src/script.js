525f8162a084e4ac6b25e4f7edd695d28acde91c
sannepetersvirtualgraphicdesigner <60850206+sannepetersvirtualgraphicdesigner@users.noreply.github.com>
2021-11-20 13:39:14 +0100

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,5 +1,9 @@
 function buyYoghurt() {
-  alert("test");
+  // alert("test");
+  let name = prompt("What is your name?");
+  let email = prompt("What is your email address?");
+  name = name.trim();
+  alert(`Thank you ${name}. We'll be in contact soon.`);
 }
 let btnBuyyoghurt = document.querySelector(".btn-buyyoghurt");
 btnBuyyoghurt.addEventListener("click", buyYoghurt);
