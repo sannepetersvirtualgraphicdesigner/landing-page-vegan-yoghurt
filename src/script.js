9e8a41b9c6f44ff414b387aeb5f7077fd2b3e207
sannepetersvirtualgraphicdesigner <60850206+sannepetersvirtualgraphicdesigner@users.noreply.github.com>
2021-11-20 13:49:24 +0100

    Update script.js

--- src/script.js
+++ src/script.js
@@ -2,8 +2,14 @@ function buyYoghurt() {
   // alert("test");
   let name = prompt("What is your name?");
   let email = prompt("What is your email address?");
-  name = name.trim();
-  alert(`Thank you ${name}. We'll be in contact soon.`);
+  name = name.trim().toLowerCase();
+  let sentence = `Thank you ${name}. We'll be in contact soon.`;
+
+  if (name.length > 0) {
+    alert(sentence);
+  } else {
+    alert(`Please fill in your name`);
+  }
 }
 let btnBuyyoghurt = document.querySelector(".btn-buyyoghurt");
 btnBuyyoghurt.addEventListener("click", buyYoghurt);
