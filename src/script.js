180be16e97ef5d3db88d62a710bf4b50c37ce84b
sannepetersvirtualgraphicdesigner <60850206+sannepetersvirtualgraphicdesigner@users.noreply.github.com>
2021-11-20 13:56:22 +0100

    Revert "Update script.js"
    This reverts commit 9e8a41b9c6f44ff414b387aeb5f7077fd2b3e207.

--- src/script.js
+++ src/script.js
@@ -2,14 +2,8 @@ function buyYoghurt() {
   // alert("test");
   let name = prompt("What is your name?");
   let email = prompt("What is your email address?");
-  name = name.trim().toLowerCase();
-  let sentence = `Thank you ${name}. We'll be in contact soon.`;
-
-  if (name.length > 0) {
-    alert(sentence);
-  } else {
-    alert(`Please fill in your name`);
-  }
+  name = name.trim();
+  alert(`Thank you ${name}. We'll be in contact soon.`);
 }
 let btnBuyyoghurt = document.querySelector(".btn-buyyoghurt");
 btnBuyyoghurt.addEventListener("click", buyYoghurt);
